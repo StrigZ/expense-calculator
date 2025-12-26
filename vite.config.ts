6f1fe5b3780ebb45e9fed9f3d27f46d8a43ac7a4
StrigZ <mirgasan200@gmail.com>
2025-12-26 17:01:59 +0300

    Add tailwindCSS

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig(({ mode }) => ({
@@ -5,4 +6,5 @@ export default defineConfig(({ mode }) => ({
 	build: {
 		sourcemap: mode === "development",
 	},
+	plugins: [tailwindcss()],
 }));
