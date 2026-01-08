603c3c945bec3deac382c78da6ea78b903817115
StrigZ <mirgasan200@gmail.com>
2026-01-08 17:20:27 +0300

    Add Button component

---
+++ src/components/button.ts
@@ -0,0 +1,49 @@
+import { Component } from "./component";
+
+type ButtonConstructor = {
+	text: string;
+	variant?: "primary" | "outline";
+	className?: string;
+	type?: "submit" | "button";
+	onClick?: (e: Event) => void;
+	disabled?: boolean;
+};
+
+export class Button extends Component<HTMLButtonElement> {
+	constructor({
+		text,
+		type,
+		variant,
+		className,
+		disabled,
+		onClick,
+	}: ButtonConstructor) {
+		const button = document.createElement("button");
+		button.classList.add("btn", "text-accent");
+
+		super(button);
+
+		this.element.textContent = text;
+		this.element.type = type ?? "button";
+		this.element.classList.add(`btn-${variant ?? "primary"}`);
+		if (onClick) {
+			this.element.addEventListener("click", onClick);
+		}
+		if (className) {
+			this.element.classList.add(...className.split(" "));
+		}
+		this.element.disabled = disabled ?? false;
+	}
+
+	disable() {
+		this.element.disabled = true;
+	}
+
+	enable() {
+		this.element.disabled = false;
+	}
+
+	render() {
+		return this.element;
+	}
+}
